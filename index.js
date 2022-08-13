7f394e2f951d53ad4d2a5b56df69209af9d0629a
Dari <dari_c95@outlook.com>
2022-08-13 18:48:53 -0300

    Agrega funcion para crear un equipo

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const datosEquipos = require('./data/equipos.json')
 const path = require('path'); 
 const methodOverride = require('method-override')
 
-const upload = multer({ dest: './uploads/imagenes' });
 const exphbs = require('express-handlebars');
 
 const PUERTO = 8084; 
@@ -24,6 +23,16 @@ app.use(methodOverride('_method'));
 
 app.use(express.static(`${__dirname}/uploads`));
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/imagenes')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+  	}
+});
+const upload = multer({ storage: storage });
+
 
 app.get('/', (req, res) => {
     res.render('home_crud', {
@@ -38,7 +47,7 @@ app.get('/', (req, res) => {
 app.get('/equipo/:id/ver', (req, res) => {
     
     let idEquipo = Number(req.param('id')); 
-    let [data] = datosEquipos.filter(equipo => equipo.id === idEquipo); 
+    let [data] = datosEquipos.filter(equipo => equipo.id === idEquipo);  
  
     res.render('arsenal', {
         layout: 'equipo',
@@ -103,7 +112,7 @@ app.put('/:id/actualizado', upload.single('imagen'), (req, res) => {
     if (fotoClub === undefined) { 
         fotoClub = data.crestUrl;
     } else { 
-        fotoClub = req.file.filename; 
+        fotoClub = req.file.originalname; 
     } 
 
     let {name, shortName, country, tla, address, phone, website, founded, email, clubColors, venue} = req.body;
@@ -111,8 +120,7 @@ app.put('/:id/actualizado', upload.single('imagen'), (req, res) => {
         id: idEquipoActualizado,
         area: {id:data.area.id, name: country},
         name,
-        shortName,
-        country,
+        shortName, 
         tla,
         crestUrl: fotoClub,
         address,
@@ -131,6 +139,51 @@ app.put('/:id/actualizado', upload.single('imagen'), (req, res) => {
     } else {
         datosEquipos.splice(index,1, infoEquipoActualizada);
     }  
+    
+    res.redirect('/');
+}); 
+
+
+app.get('/crear', (req, res) => {
+    res.render('crear_equipo', {
+        layout: 'crear' 
+    })
+}); 
+
+
+app.post('/nuevo-equipo', upload.single('imagen'), (req, res) => {
+    
+    let ultimoEquipoDelArray = datosEquipos[datosEquipos.length - 1];
+    let idNuevoEquipo = ultimoEquipoDelArray.id + 1;
+
+    let fotoClub = req.file;
+
+    if (fotoClub === undefined) { 
+        return;
+    } else { 
+        fotoClub = req.file.originalname; 
+    } 
+    
+    
+    let {name, shortName, country, tla, address, phone, website, founded, email, clubColors, venue} = req.body;
+    let infoNuevoEquipo = {
+        id: idNuevoEquipo,
+        area: {id:2072, name: country},
+        name,
+        shortName, 
+        tla,
+        crestUrl: fotoClub,
+        address,
+        phone,
+        website,
+        email,
+        founded,
+        clubColors,
+        venue,
+        lastUpdated: new Date().toISOString()
+    };
+    
+    datosEquipos.push(infoNuevoEquipo); 
     
     res.redirect('/');
 }); 
